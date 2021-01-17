cc2c5b2dcb822f37633ed852e92cff4279a7c89b
Ramzi-B <Ramzi.B@gmx.com>
2021-01-17 18:22:49 +0100

    delete the folder config, unused functions, update files

--- inc/utils.php
+++ inc/utils.php
@@ -10,27 +10,6 @@ function isAuthenticated()
     return isset($_SESSION['auth']) && $_SESSION['auth'] == true;
 }
 
-function checkIntegerRange($int, $min, $max)
-{
-    if (is_string($int) && !ctype_digit($int)) {
-        // contains non digit characters
-        return false;
-    }
-
-    if (!is_int((int) $int)) {
-        // other non integer value or exceeds PHP_MAX_INT
-        return false;
-    }
-
-    return ($int >= $min && $int <= $max);
-}
-
-
-// function getConfig($fileName)
-// {
-//     return include ROOT_PATH . "/../config/{$fileName}.php";
-// }
-
 function startSession()
 {
     if (session_status() == PHP_SESSION_NONE) {
